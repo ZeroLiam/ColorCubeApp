fddb0eacb124a396eb3445e6af96a60fe859d424
ZeroLiam <liselotr@gmail.com>
2017-09-26 09:50:36 +0200

    Content for Home, Constructive, Expressive, and Impressive added

--- colorcube/src/containers/Expressive.js
+++ colorcube/src/containers/Expressive.js
@@ -10,6 +10,21 @@ class Expressive extends Component {
     return (
       <div className="Home">
         <Header title="Expressive" />
+
+        <AboutMore>
+            <p className="text-left">
+            In the the Theory of Color Expression Itten said that color effects besides being physical and chemical generate also psychological responses, affecting principal areas of mental and emotional experience.
+            </p>
+
+            <p className="text-left">
+            Taking into account Goethe&#8217;s Color theory. Itten expressed that color can transmit different emotions related with it. For that it is very important to consider and define the specific hue of the color and consider the context in which it is situated.
+            </p>
+
+            <p className="text-right">
+              <i>The majestic cycle of respiration performed by nature in these four phases can hence be clearly and objectively represented in color: but unless we apply reason to the choice of color combinations, keeping the entire universe of color before us, we shall find none but private solutions and miss those of general truth and validity.</i><br/>Itten 1969, p84.
+            </p>
+        </AboutMore>
+
         <CubeContainer type="expressive" />
       </div>
     );
