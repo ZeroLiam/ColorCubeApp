9f1fc3102fe21415ef59b75b753def7b70372c43
ZeroLiam <liselotr@gmail.com>
2017-09-26 09:44:25 +0200

    Added CSS for overlapping header, menu, and cube

--- colorcube/src/containers/Expressive.js
+++ colorcube/src/containers/Expressive.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Header from "./../components/Header";
 import CubeContainer from "./../components/CubeContainer";
+import AboutMore from './../components/AboutMore';
+import "./../styles/components_styles.css";
+import img3 from './../assets/ex/003.jpg';
 
 class Expressive extends Component {
   render() {
--- colorcube/src/containers/Impressive.js
+++ colorcube/src/containers/Impressive.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 import Header from "./../components/Header";
 import CubeContainer from "./../components/CubeContainer";
+import AboutMore from './../components/AboutMore';
+import "./../styles/components_styles.css";
+import img3 from './../assets/ex/003.jpg';
 
 class Impressive extends Component {
   render() {
     return (
       <div className="Home">
         <Header title="Impressive" />
+
+        <AboutMore>
+            <p className="text-left">
+            &#34;<i>The theory of color impression properly begins with the coor effects in nature. That is, that we investigate the impressions made by colored objects on our sense of vision.</i>&#34; Itten 1969 p.79.
+            </p>
+
+            <p className="text-left">
+            As Itten said that Color and its effects are in the eye of the beholder he explains that contrast appears when distinct differences can be perceived between two compared effects. When such differences approach to their maximal degree they can be defined as diametrical or polar contrasts.
+            </p>
+
+            <p className="text-left">
+            He defined seven kinds of color contrasts:
+            </p>
+
+            <p className="text-left">
+            1- Contrast of hue<br/>
+            2- Light dark contrast<br/>
+            3- Cold-warm contrast<br/>
+            3- Complementary contrast<br/>
+            4- Simultaneous contrast<br/>
+            6- Contrast of saturation<br/>
+            7- Contrast of extension<br/>
+            </p>
+
+            <p className="text-center">
+                <img src={img3} alt="003.jpg" />
+            </p>
+            
+            <p className="text-left">
+            It is important to say that Itten suggested this color contrast based on his color circle. Nevertheless in this case order of the colors in the circle used for the interactions is one based in the theory Harold Küpers, who made some critics on Itten’s theory. He considers as primary colors Red, Green and Blue for the additive mix and Cyan, Yellow and Magenta for the subtractive mix. Furthermore the order of the colors in the circle suggested coincide with the order of the colors in the RGB model, and are useful to prove Itten&#8217;s Impression Theory.
+            </p>
+        </AboutMore>
+
         <CubeContainer type="impressive" />
       </div>
     );
